38a20238c7e600d44ffd6df29a06ede0eb309534
TKTEE <tonykaranja15@gmail.com>
2023-06-12 08:31:07 -0400

    farmease

---
+++ app/src/main/java/com/example/farmease/ViewsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.farmease
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_views)
+    }
+}
